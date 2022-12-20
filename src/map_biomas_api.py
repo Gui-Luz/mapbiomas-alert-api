352f87f966873b307a126b7dd792df0cf8c3f681
Gui-Luz <guilhermeluzsaldanha@gmail.com>
2022-12-20 12:27:41 -0300

    first commit

---
+++ src/map_biomas_api.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+
+import requests
+from src.queries import territories_query, alert_report_query, published_alerts_query, alerts_from_car_query, \
+    published_alert_query, mutation
+
+
+class MapBiomasAlertApi(object):
+    ENDPOINT = 'https://plataforma.alerta.mapbiomas.org/api/graphql'
+    TIMEOUT = 120
+    MUTATION_QUERY = mutation
+    PUBLISHED_ALERTS_QUERY = published_alerts_query
+    PUBLISHED_ALERT_QUERY = published_alert_query
+    ALERT_REPORT_QUERY = alert_report_query
+    TERRITORIES_QUERY = territories_query
+    ALERTS_FROM_CAR_QUERY = alerts_from_car_query
+
+    @classmethod
+    def token(cls, credentials):
+        request = requests.post(MapBiomasAlertApi.ENDPOINT, json={'query': cls.MUTATION_QUERY, 'variables': credentials},
+                                timeout=MapBiomasAlertApi.TIMEOUT)
+
+        if request.status_code == 200:
+            result = request.json()
+            if 'errors' in result:
+                msg = '\n'.join([error['message'] for error in result['errors']])
+                raise Exception(msg)
+            else:
+                return result["data"]["signIn"]["token"]
+        else:
+            msg = "Failed to run by returning code of {}.".format(request.status_code)
+            raise Exception(msg)
+
+    @classmethod
+    def query(cls, token, query, filters=dict()):
+
+        headers = {'Authorization': 'Bearer ' + token}
+
+        request = requests.post(cls.ENDPOINT, json={'query': query, 'variables': filters}, headers=headers,
+                          timeout=cls.TIMEOUT)
+
+        if request.status_code == 200:
+            result = request.json()
+            if 'errors' in result:
+                msg = '\n'.join([error['message'] for error in result['errors']])
+                raise Exception(msg)
+            else:
+                return result
+        else:
+            msg = "Failed to run by returning code of {}.".format(request.status_code)
+            raise Exception(msg)
+
+
+
---
+++ src/queries.py
@@ -0,0 +1,203 @@
+mutation = \
+    """mutation($email: String!, $password: String!)
+        { 
+        signIn(email: $email, password: $password)
+            {
+                token
+            }
+        }
+    """
+
+territories_query = \
+    """
+    query
+    (
+        $category: TerritoryCategoryEnum
+    )
+    {
+    territories
+    (
+        category: $category
+    )
+        {
+            id
+            name
+        }
+    }
+    """
+
+published_alerts_query = \
+    """
+    query
+    (
+      $startDetectedAt: String
+      $endDetectedAt: String
+      $startPublishedAt: String
+      $endPublishedAt: String
+      $territoryIds: [Int!]
+      $limit: Int
+      $offset: Int
+    )
+    {
+    publishedAlerts 
+    (
+        startDetectedAt: $startDetectedAt
+        endDetectedAt: $endDetectedAt
+        startPublishedAt: $startPublishedAt
+        endPublishedAt: $endPublishedAt
+        territoryIds: $territoryIds
+        limit: $limit
+        offset: $offset
+    )
+        {
+            alertCode
+            alertInsertedAt
+            areaHa
+            cars { carCode, id }
+            detectedAt
+            geometry { geom }
+            id
+            source
+        }
+    }
+    """
+
+published_alert_query = \
+    """
+    query published_alert($alertCode: String!) {
+      publishedAlert(alertCode: $alertCode) {
+        alertCode
+        areaHa
+        sources
+        bbox
+        alertBiomes
+        alertCities
+        alertStates
+        detectedAt
+        publishedAt
+        deforestationClass
+      }
+    }
+    """
+
+
+alert_report_query = \
+    """
+    query
+    (
+      $alertCode: Int!
+      $carId: Int
+    )
+    {
+      alertReport(alertCode: $alertCode, carId: $carId) {
+        alertAreaInCar
+        alertCode
+        alertGeomWkt
+        areaHa
+        carCode
+        changes {
+          labels
+        #   layer
+          overYears {
+            imageUrl
+            year
+          }
+        }
+        # imageGridMeasurements {
+        #   latitude {
+            # startCoordinate
+            # endCoordinate
+            # steps {
+            #   step
+            #   stepCoordinate
+            # }
+        #   }
+        #   longitude {
+            # startCoordinate
+            # endCoordinate
+            # steps {
+            #   step
+            #   stepCoordinate
+            # }
+        #   }
+        # }
+        images {
+          before {
+            acquiredAt
+            satellite
+            url
+          }
+          after {
+            acquiredAt
+            satellite
+            url
+          }
+        #   labels
+          alertInProperty
+          propertyInState
+        }
+        intersections {
+          conservationUnits {
+            area
+            # count
+          }
+          deforestmentsAuthorized {
+            area
+            # count
+          }
+          forestManagements {
+            area
+            # count
+          }
+          indigenousLands {
+            area
+            # count
+          }
+          settlements {
+            area
+            # count
+          }
+          withRuralProperty {
+            embargoes {
+              area
+            #   count
+            }
+            legalReserves {
+              area
+            #   count
+            }
+            permanentProtected {
+              area
+            #   count
+            }
+            riverSources {
+            #   area
+              count
+            }
+          }
+        }
+        simplifiedPoints {
+          imageUrl
+          table {
+            number
+            x
+            y
+          }
+        }
+        source
+        territories {
+          categoryName
+          id
+          name
+        }
+        # warnings
+      }
+    }
+    """
+
+alerts_from_car_query = \
+    """
+    query alert_from_car($carCode:String!) {
+      alertsFromCar(carCode:$carCode) 
+    }
+    """
